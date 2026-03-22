a742985ae960410a65de4cbe1f3a4fee17d3e1c9
root <ehab.saleh@lrz.de>
2026-03-22 09:40:24 +0100

    add headers

---
+++ include/fileio.h
@@ -0,0 +1,19 @@
+#define _GNU_SOURCE
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <errno.h>
+
+#define ALIGNMENT 4096
+
+inline double now_sec() {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec + ts.tv_nsec * 1e-9;
+}
+
+void usage(char *prog);
+
